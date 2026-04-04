054e2671ad37114049945ce318f316d44e1a382e
Hamza <mhamza111315@gmail.com>
2026-04-04 17:50:10 +0200

    install script

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo pacman -S --needed gnome-keyring rofi swaync nwg-look pavucontrol blueman z
   ttf-fira-code ttf-sourcecodepro-nerd \
   adw-gtk-theme ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick \
   breeze breeze-icons qt6ct qt5ct kvantum kvantum-qt5 \
-  git base-devel hyprpaper hyprlock hypridle hyprpolkitagent clipvault python-pip polkit-kde-agent \
+  git base-devel hyprpaper hyprlock hypridle hyprpolkitagent python-pip polkit-kde-agent \
   spotify-launcher
 
 echo "Installing yay (AUR helper)..."
@@ -25,7 +25,7 @@ fi
 
 echo "Installing AUR packages..."
 
-yay -S --needed nerd-fonts-jetbrains-mono nerd-fonts-fira-code nerd-fonts-hack ttf-jetbrains-mono-nerd wlogout
+yay -S --needed nerd-fonts-jetbrains-mono nerd-fonts-fira-code nerd-fonts-hack ttf-jetbrains-mono-nerd wlogout clipvault
 
 
 ./install_fish.sh
