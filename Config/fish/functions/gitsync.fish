b17592f73181e353e936e2470b8c2f18b3d3d139
Hamza <mhamza111315@gmail.com>
2026-06-28 00:26:54 +0200

    Update dotfiles: 2026-06-28 12:26

---
+++ Config/fish/functions/gitsync.fish
@@ -0,0 +1,3 @@
+function gitsync --wraps=/home/ham/.config/hypr/scripts/gitsync.sh --description 'alias gitsync /home/ham/.config/hypr/scripts/gitsync.sh'
+    /home/ham/.config/hypr/scripts/gitsync.sh $argv
+end
