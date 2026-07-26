b85bcc160c36817144891a79a52c6c19e0cd2a88
Hamza Aslam <mhamza111315@gmail.com>
2026-07-26 23:35:10 +0200

    Update dotfiles: 2026-07-26 11:35

--- Config/hypr/scripts/gitsync.sh
+++ Config/hypr/scripts/gitsync.sh
@@ -40,7 +40,9 @@ if [[ "$answer" =~ ^[Yy]$ ]]; then
   if [ -n "$(git status --porcelain)" ]; then
     git add .
     git commit -m "Update dotfiles: $(date '+%Y-%m-%d %I:%M')"
+    echo "      ========== git pull ==========    "
     git pull --rebase
+    echo "      ========== git push ==========    "
     git push
   else
     echo "No changes to commit."
